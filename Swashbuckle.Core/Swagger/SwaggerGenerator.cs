5f83317a71faba3f03fd0071ca4df2d7cb833f33
scherbinin semyon <s.scherbinin@parcsis.org>
2015-12-17 17:43:26 +0400

    Add: group by MethodInfo

--- Swashbuckle.Core/Swagger/SwaggerGenerator.cs
+++ Swashbuckle.Core/Swagger/SwaggerGenerator.cs
@@ -5,190 +5,193 @@ using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using System.Net.Http.Formatting;
+using System.Web.Http.Controllers;
 
 namespace Swashbuckle.Swagger
 {
-    public class SwaggerGenerator : ISwaggerProvider
-    {
-        private readonly IApiExplorer _apiExplorer;
-        private readonly JsonSerializerSettings _jsonSerializerSettings;
-        private readonly IDictionary<string, Info> _apiVersions;
-        private readonly SwaggerGeneratorOptions _options;
+	public class SwaggerGenerator : ISwaggerProvider
+	{
+		private readonly IApiExplorer _apiExplorer;
+		private readonly JsonSerializerSettings _jsonSerializerSettings;
+		private readonly IDictionary<string, Info> _apiVersions;
+		private readonly SwaggerGeneratorOptions _options;
 
-        public SwaggerGenerator(
-            IApiExplorer apiExplorer,
-            JsonSerializerSettings jsonSerializerSettings,
-            IDictionary<string, Info> apiVersions,
-            SwaggerGeneratorOptions options = null)
-        {
-            _apiExplorer = apiExplorer;
-            _jsonSerializerSettings = jsonSerializerSettings;
-            _apiVersions = apiVersions;
-            _options = options ?? new SwaggerGeneratorOptions();
-        }
+		public SwaggerGenerator(
+			IApiExplorer apiExplorer,
+			JsonSerializerSettings jsonSerializerSettings,
+			IDictionary<string, Info> apiVersions,
+			SwaggerGeneratorOptions options = null)
+		{
+			_apiExplorer = apiExplorer;
+			_jsonSerializerSettings = jsonSerializerSettings;
+			_apiVersions = apiVersions;
+			_options = options ?? new SwaggerGeneratorOptions();
+		}
 
-        public SwaggerDocument GetSwagger(string rootUrl, string apiVersion)
-        {
-            var schemaRegistry = new SchemaRegistry(
-                _jsonSerializerSettings,
-                _options.CustomSchemaMappings,
-                _options.SchemaFilters,
-                _options.ModelFilters,
-                _options.IgnoreObsoleteProperties,
-                _options.SchemaIdSelector,
-                _options.DescribeAllEnumsAsStrings,
-                _options.DescribeStringEnumsInCamelCase);
+		public SwaggerDocument GetSwagger(string rootUrl, string apiVersion)
+		{
+			var schemaRegistry = new SchemaRegistry(
+				_jsonSerializerSettings,
+				_options.CustomSchemaMappings,
+				_options.SchemaFilters,
+				_options.ModelFilters,
+				_options.IgnoreObsoleteProperties,
+				_options.SchemaIdSelector,
+				_options.DescribeAllEnumsAsStrings,
+				_options.DescribeStringEnumsInCamelCase);
 
-            Info info;
-            _apiVersions.TryGetValue(apiVersion, out info);
-            if (info == null)
-                throw new UnknownApiVersion(apiVersion);
+			Info info;
+			_apiVersions.TryGetValue(apiVersion, out info);
+			if (info == null)
+				throw new UnknownApiVersion(apiVersion);
 
-            var paths = GetApiDescriptionsFor(apiVersion)
-                .Where(apiDesc => !(_options.IgnoreObsoleteActions && apiDesc.IsObsolete()))
-                .OrderBy(_options.GroupingKeySelector, _options.GroupingKeyComparer)
-                .GroupBy(apiDesc => apiDesc.RelativePathSansQueryString())
-                .ToDictionary(group => "/" + group.Key, group => CreatePathItem(group, schemaRegistry));
+			var paths = GetApiDescriptionsFor(apiVersion)
+				.Where(apiDesc => !(_options.IgnoreObsoleteActions && apiDesc.IsObsolete()))
+				.OrderBy(_options.GroupingKeySelector, _options.GroupingKeyComparer)
+				.GroupBy(dd => (dd.ActionDescriptor as ReflectedHttpActionDescriptor).MethodInfo)
+				.Select(g => g.OrderBy(d => d.RelativePath.Length).First())
+				.GroupBy(apiDesc => apiDesc.RelativePathSansQueryString())
+				.ToDictionary(group => "/" + group.Key, group => CreatePathItem(group, schemaRegistry));
 
-            var rootUri = new Uri(rootUrl);
-            var port = (!rootUri.IsDefaultPort) ? ":" + rootUri.Port : string.Empty;
+			var rootUri = new Uri(rootUrl);
+			var port = (!rootUri.IsDefaultPort) ? ":" + rootUri.Port : string.Empty;
 
-            var swaggerDoc = new SwaggerDocument
-            {
-                info = info,
-                host = rootUri.Host + port,
-                basePath = (rootUri.AbsolutePath != "/") ? rootUri.AbsolutePath : null,
-                schemes = (_options.Schemes != null) ? _options.Schemes.ToList() : new[] { rootUri.Scheme }.ToList(),
-                paths = paths,
-                definitions = schemaRegistry.Definitions,
-                securityDefinitions = _options.SecurityDefinitions
-            };
+			var swaggerDoc = new SwaggerDocument
+			{
+				info = info,
+				host = rootUri.Host + port,
+				basePath = (rootUri.AbsolutePath != "/") ? rootUri.AbsolutePath : null,
+				schemes = (_options.Schemes != null) ? _options.Schemes.ToList() : new[] { rootUri.Scheme }.ToList(),
+				paths = paths,
+				definitions = schemaRegistry.Definitions,
+				securityDefinitions = _options.SecurityDefinitions
+			};
 
-            foreach(var filter in _options.DocumentFilters)
-            {
-                filter.Apply(swaggerDoc, schemaRegistry, _apiExplorer);
-            }
+			foreach (var filter in _options.DocumentFilters)
+			{
+				filter.Apply(swaggerDoc, schemaRegistry, _apiExplorer);
+			}
 
-            return swaggerDoc;
-        }
+			return swaggerDoc;
+		}
 
-        private IEnumerable<ApiDescription> GetApiDescriptionsFor(string apiVersion)
-        {
-            return (_options.VersionSupportResolver == null)
-                ? _apiExplorer.ApiDescriptions
-                : _apiExplorer.ApiDescriptions.Where(apiDesc => _options.VersionSupportResolver(apiDesc, apiVersion));
-        }
+		private IEnumerable<ApiDescription> GetApiDescriptionsFor(string apiVersion)
+		{
+			return (_options.VersionSupportResolver == null)
+				? _apiExplorer.ApiDescriptions
+				: _apiExplorer.ApiDescriptions.Where(apiDesc => _options.VersionSupportResolver(apiDesc, apiVersion));
+		}
 
-        private PathItem CreatePathItem(IEnumerable<ApiDescription> apiDescriptions, SchemaRegistry schemaRegistry)
-        {
-            var pathItem = new PathItem();
+		private PathItem CreatePathItem(IEnumerable<ApiDescription> apiDescriptions, SchemaRegistry schemaRegistry)
+		{
+			var pathItem = new PathItem();
 
-            // Group further by http method
-            var perMethodGrouping = apiDescriptions
-                .GroupBy(apiDesc => apiDesc.HttpMethod.Method.ToLower());
+			// Group further by http method
+			var perMethodGrouping = apiDescriptions
+				.GroupBy(apiDesc => apiDesc.HttpMethod.Method.ToLower());
 
-            foreach (var group in perMethodGrouping)
-            {
-                var httpMethod = group.Key;
+			foreach (var group in perMethodGrouping)
+			{
+				var httpMethod = group.Key;
 
-                var apiDescription = (group.Count() == 1)
-                    ? group.First()
-                    : _options.ConflictingActionsResolver(group);
+				var apiDescription = (group.Count() == 1)
+					? group.First()
+					: _options.ConflictingActionsResolver(group);
 
-                switch (httpMethod)
-                {
-                    case "get":
-                        pathItem.get = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "put":
-                        pathItem.put = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "post":
-                        pathItem.post = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "delete":
-                        pathItem.delete = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "options":
-                        pathItem.options = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "head":
-                        pathItem.head = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                    case "patch":
-                        pathItem.patch = CreateOperation(apiDescription, schemaRegistry);
-                        break;
-                }
-            }
+				switch (httpMethod)
+				{
+					case "get":
+						pathItem.get = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "put":
+						pathItem.put = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "post":
+						pathItem.post = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "delete":
+						pathItem.delete = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "options":
+						pathItem.options = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "head":
+						pathItem.head = CreateOperation(apiDescription, schemaRegistry);
+						break;
+					case "patch":
+						pathItem.patch = CreateOperation(apiDescription, schemaRegistry);
+						break;
+				}
+			}
 
-            return pathItem;
-        }
+			return pathItem;
+		}
 
-        private Operation CreateOperation(ApiDescription apiDescription, SchemaRegistry schemaRegistry)
-        {
-            var parameters = apiDescription.ParameterDescriptions
-                .Select(paramDesc =>
-                    {
-                        var inPath = apiDescription.RelativePathSansQueryString().Contains("{" + paramDesc.Name + "}");
-                        return CreateParameter(paramDesc, inPath, schemaRegistry);
-                    })
-                 .ToList();
+		private Operation CreateOperation(ApiDescription apiDescription, SchemaRegistry schemaRegistry)
+		{
+			var parameters = apiDescription.ParameterDescriptions
+				.Select(paramDesc =>
+					{
+						var inPath = apiDescription.RelativePathSansQueryString().Contains("{" + paramDesc.Name + "}");
+						return CreateParameter(paramDesc, inPath, schemaRegistry);
+					})
+				 .ToList();
 
-            var responses = new Dictionary<string, Response>();
-            var responseType = apiDescription.ResponseType();
-            if (responseType == null || responseType == typeof(void))
-                responses.Add("204", new Response { description = "No Content" });
-            else
-                responses.Add("200", new Response { description = "OK", schema = schemaRegistry.GetOrRegister(responseType) });
+			var responses = new Dictionary<string, Response>();
+			var responseType = apiDescription.ResponseType();
+			if (responseType == null || responseType == typeof(void))
+				responses.Add("204", new Response { description = "No Content" });
+			else
+				responses.Add("200", new Response { description = "OK", schema = schemaRegistry.GetOrRegister(responseType) });
 
-            var operation = new Operation
-            { 
-                tags = new [] { _options.GroupingKeySelector(apiDescription) },
-                operationId = apiDescription.FriendlyId(),
-                produces = apiDescription.Produces().ToList(),
-                consumes = apiDescription.Consumes().ToList(),
-                parameters = parameters.Any() ? parameters : null, // parameters can be null but not empty
-                responses = responses,
-                deprecated = apiDescription.IsObsolete()
-            };
+			var operation = new Operation
+			{
+				tags = new[] { _options.GroupingKeySelector(apiDescription) },
+				operationId = apiDescription.FriendlyId(),
+				produces = apiDescription.Produces().ToList(),
+				consumes = apiDescription.Consumes().ToList(),
+				parameters = parameters.Any() ? parameters : null, // parameters can be null but not empty
+				responses = responses,
+				deprecated = apiDescription.IsObsolete()
+			};
 
-            foreach (var filter in _options.OperationFilters)
-            {
-                filter.Apply(operation, schemaRegistry, apiDescription);
-            }
+			foreach (var filter in _options.OperationFilters)
+			{
+				filter.Apply(operation, schemaRegistry, apiDescription);
+			}
 
-            return operation;
-        }
+			return operation;
+		}
 
-        private Parameter CreateParameter(ApiParameterDescription paramDesc, bool inPath, SchemaRegistry schemaRegistry)
-        {
-            var @in = (inPath)
-                ? "path"
-                : (paramDesc.Source == ApiParameterSource.FromUri) ? "query" : "body";
+		private Parameter CreateParameter(ApiParameterDescription paramDesc, bool inPath, SchemaRegistry schemaRegistry)
+		{
+			var @in = (inPath)
+				? "path"
+				: (paramDesc.Source == ApiParameterSource.FromUri) ? "query" : "body";
 
-            var parameter = new Parameter
-            {
-                name = paramDesc.Name,
-                @in = @in
-            };
+			var parameter = new Parameter
+			{
+				name = paramDesc.Name,
+				@in = @in
+			};
 
-            if (paramDesc.ParameterDescriptor == null)
-            {
-                parameter.type = "string";
-                parameter.required = true;
-                return parameter; 
-            }
+			if (paramDesc.ParameterDescriptor == null)
+			{
+				parameter.type = "string";
+				parameter.required = true;
+				return parameter;
+			}
 
-            parameter.required = inPath || !paramDesc.ParameterDescriptor.IsOptional;
-            parameter.@default = paramDesc.ParameterDescriptor.DefaultValue;
+			parameter.required = inPath || !paramDesc.ParameterDescriptor.IsOptional;
+			parameter.@default = paramDesc.ParameterDescriptor.DefaultValue;
 
-            var schema = schemaRegistry.GetOrRegister(paramDesc.ParameterDescriptor.ParameterType);
-            if (parameter.@in == "body")
-                parameter.schema = schema;
-            else
-                parameter.PopulateFrom(schema);
+			var schema = schemaRegistry.GetOrRegister(paramDesc.ParameterDescriptor.ParameterType);
+			if (parameter.@in == "body")
+				parameter.schema = schema;
+			else
+				parameter.PopulateFrom(schema);
 
-            return parameter;
-        }
-    }
+			return parameter;
+		}
+	}
 }
